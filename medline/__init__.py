3044ccd4cbfc97285c81addeef11ccf139b93fcf
dwangd5 <dwangd5@yahoo.com>
2020-03-04 12:18:37 -0600

    initial commit

---
+++ medline/__init__.py
@@ -0,0 +1,8 @@
+"""
+Medline Parser: A Python Parser for PubMed MEDLINE XML Dataset
+Author: D Wang
+Adapted from: Titipat Achakulvisut, Daniel E. Acuna, github: https://github.com/titipata/pubmed_parser
+"""
+__version__ = "0.1.0"
+from .parser import parse_medline_xml, parse_medline_grant_id
+from .utils import pretty_print
